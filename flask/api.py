bac070addedd985d311f6b76a24ba40475d9d2e0
ag0r <ag0r@users.noreply.github.com>
2022-07-15 10:12:45 -0500

    Initial commit of api definition for testing purposes

--- flask/api.py
+++ flask/api.py
@@ -1,7 +1,8 @@
-from flask import Flask, Response
+from flask import Flask
 from flask_restx import Api, Resource
-from flask import request
+from flask import request, Response
 from pymongo import MongoClient
+import json
 import os
 
 app = Flask(__name__)
@@ -12,15 +13,35 @@ client = MongoClient()
 @api.route('/runcv', methods=['POST', 'GET'])
 class parse_cv(Resource):
     def post(self):
-        print(request.files, flush=True)
-        cv = {
-        "x1": 0.30,
-        "y1": 0.47,
-        "x_off": 0.334,
-        "y_off": 0.34
+        r = {
+          "boxes": [
+            {
+              "x1": 0.30,
+              "y1": 0.47,
+              "x_off": 0.334,
+              "y_off": 0.34
+            }
+          ]
+          #],
+          #"errors": [
+          #  {
+          #    "type": "overlap",
+          #    "boxIndexes": [0]
+          #  }
+          #]
         }
 
-        return Response(cv, status=200, mimetype='application/json')
+        cv =  {
+            "x1": 0.30,
+            "y1": 0.47,
+            "x_off": 0.334,
+            "y_off": 0.34
+            }
+
+
+        print(json.dumps(r), flush=True)
+
+        return Response(json.dumps(cv), status=200, mimetype="application/json")
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
